593c21f2fa556ffbf336f4a9226e5320e4c2bfae
JamieHa8794 <JamieHa8794@gmail.com>
2022-10-04 17:57:37 -0700

    initial database set up done

---
+++ db/index.js
@@ -0,0 +1,37 @@
+const Sequelize = require('sequelize');
+const {STRING} = Sequelize
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/lavander-oak');
+
+const User = db.define('user', {
+    name: {
+        type: STRING
+    }
+})
+
+const Furniture = db.define('furniture', {
+    name: {
+        type: STRING
+    }
+})
+
+const syncAndSeed = async () =>{
+    try{
+        await db.sync({force: true})
+        const [ross, rachel, monica, chandler, pheobe, joey] = Promise.all(['Ross', 'Rachel', 'Monica', 'Chandler', 'Pheobe', 'Joey'].map(name => User.create({name})))
+        const [chair, sofa, bed, lamp, rug] = Promise.all(['chair', 'sofa', 'bed', 'lamp', 'rug'].map(name => Furniture.create({name})))
+        console.log('connected to db')
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+module.exports = {
+    db,
+    syncAndSeed,
+    model: {
+        Furniture,
+        User
+    }
+}
+
