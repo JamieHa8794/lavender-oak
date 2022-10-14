9d6af3fa5b87b3455972cb5c25b1d695307cfe0a
JamieHa8794 <JamieHa8794@gmail.com>
2022-10-13 21:21:08 -0400

    cart functionality & some styles

--- src/Cart.js
+++ src/Cart.js
@@ -2,13 +2,34 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 
-const Cart = ({carts})=>{
+const Cart = ({carts, products})=>{
 
+    
+    const userId = 'e20feb6c-e0d7-4ca2-95c6-27a1a201f527';
 
-    const userId = '348f74db-3b8e-4a14-808b-b8dc41719db7';
-    const cartItems = carts.filter(cartItem => cartItem.userId === userId);
+    const cartItems = carts.filter(cartItem => cartItem.userId == userId);
+    
 
-    if(cartItems.length = 0){
+    const productList = [];
+    cartItems.map(cartItem =>{
+        products.map(product =>{
+            if(cartItem.productId == product.id){
+                productList.push(product);
+            }
+        })
+    })
+
+    console.log(productList)
+
+    let sum = 0;
+
+    productList.map(product =>{
+        console.log(sum)
+        sum = sum + (product.price *1)
+    })
+    
+
+    if(cartItems.length == 0){
         return(
             <div>
                 Cart is empty
@@ -17,9 +38,30 @@ const Cart = ({carts})=>{
     }
     return(
         <div>
+            <div>
+                Items in Cart: 
+            </div>
             <ul>
-                {}
+                {productList.map(product =>{
+                    return(
+                        <li className='cart-items-li'>
+                            <img src={product.img} className='cart-items-img'/>
+                            <div className='cart-items-product-name'>
+                                {product.name}
+                            </div>
+                            <div>
+                                ${product.price}.00
+                            </div>
+                        </li>
+                    )
+                })}
             </ul>
+
+            <div>
+                <div>
+                    Subtotal: {sum ? `$${sum}.00` : 0}
+                </div>
+            </div>
         </div>
     )
 }
--- src/SingleProduct.js
+++ src/SingleProduct.js
@@ -13,7 +13,7 @@ class SingleProduct extends Component{
     }
     onClick(){
         const user = {
-            id: '16b9e1f1-0983-41c2-9f7b-578ee4a053c3'
+            id: 'e20feb6c-e0d7-4ca2-95c6-27a1a201f527'
         }
         const {products, match} = this.props
         const productId = match.params.id;
@@ -32,9 +32,7 @@ class SingleProduct extends Component{
         console.log(productId)
         const product = products.find(product => product.id === productId)
         console.log(product)
-        const user = {
-            id: '17c04d39-1acf-4581-8f4c-224143461e2f'
-        }
+        
         if(!product){
             console.log('here')
             return(
@@ -74,57 +72,6 @@ class SingleProduct extends Component{
 
 }
 
-
-// const SingleProduct = ({products, match, addToCart}) =>{
-//     const productId = match.params.id;
-//     console.log(productId)
-//     const product = products.find(product => product.id === productId)
-//     console.log(product)
-//     const user = {
-//         id: '17c04d39-1acf-4581-8f4c-224143461e2f'
-//     }   
-
-//     if(!product){
-//         console.log('here')
-//         return(
-//         <div>
-//             Whoops nothing to see here...
-//             <Link to='/products'>Go back to all products...</Link>
-//         </div>
-//         )
-//     }
-
-//     const onClick = (event) =>{
-//         console.log(this)
-//         addToCart(user.id, product.id)
-//         console.log(user.id, product.id)
-//     }
-//     return(
-//         <div>
-//         <div className='single-product-back-to-products'>
-//             /<Link to='/products'>Products</Link>/<Link to={`/products/${product.category}`}>{product.category}s</Link>
-//         </div>
-//         <div className='single-product-container'>
-//             <img src={product.img} className='single-product-img'/>
-//             <div className='single-product-info-container'>
-//                 <div className='single-product-name'>
-//                     {product.name}
-//                 </div>
-//                 <div className='single-product-category'>
-//                     {product.category}
-//                 </div>
-//                 <div className='single-product-price'>
-//                     ${product.price}.00
-//                 </div>
-//                 <div className='single-product-short_description'>
-//                     {product.short_description}
-//                 </div>
-//             <button onClick={onClick} className='single-product-add-to-cart'>Add to Cart</button>
-//             </div>
-//         </div>
-//     </div>
-//     )
-// }
 
 const mapStateToProps = (state) =>{
     return state;
