6d14bc08533cdce35909c6d2cb26cf833cc2eec1
JamieHa8794 <JamieHa8794@gmail.com>
2022-12-02 00:44:58 -0500

    styling myProfile

--- src/MyProfile.js
+++ src/MyProfile.js
@@ -3,6 +3,9 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
 import Paper from '@mui/material/Paper';
+import Divider from '@mui/material/Divider';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 
 const MyProfile = ({users, auth}) =>{
@@ -28,43 +31,102 @@ const MyProfile = ({users, auth}) =>{
             <Paper elevation={3} >
 
                 <div className="myProfile-container">
+                
+                    <div className="myProfile-header">My Profile</div>
+                    <Link className='myProfile-edit-link' to='/myProfile/editInfo'>Edit Personal Info</Link>
                     <div className="myProfile-personal-detail-container">
-                        <div>Personal Details</div>
-                        <div>
-                        <div className="myProfile-info-firstName">
-                            {user.firstName}
+                        <div className="myProfile-personal-detail-header">
+                            Personal Details
                         </div>
-                        <div className="myProfile-info-middleName">
-                            {user.middleName}
-                        </div>
-                        <div className="myProfile-info-lastName">
-                            {user.lastName}
-                        </div>
+                        <Divider/>
+                        <div className="myProfile-personal-detail-container-sub">
+                            <TextField
+                                className="myProfile-info"
+                                id="first-name-read-only-input"
+                                label="First Name"
+                                defaultValue={user.firstName}
+                                InputProps={{
+                                    readOnly: true,
+                                }}
+                                sx={{margin: 1}}
+
+                                variant="filled"                            
+                            />
+                            <TextField
+                                className="myProfile-info"
+                                id="middle-name-read-only-input"
+                                label="Middle Name"
+                                defaultValue={user.middleName}
+                                InputProps={{
+                                    readOnly: true,
+                                }}
+                                sx={{margin: 1}}
+                                variant="filled"                            
+                            />
+                            <TextField
+                                className="myProfile-info"
+                                id="last-name-read-only-input"
+                                label="Last Name"
+                                defaultValue={user.lastName}
+                                InputProps={{
+                                    readOnly: true,
+                                }}
+                                sx={{margin: 1}}
+                                variant="filled"                            
+                            />
                         </div>
                     </div>
-
-
-
-
-
-
-                    <Link to='/myProfile/editInfo'>Edit Personal Info</Link>
-                    <img src={user.img} className='myProfile-img'/>
-                    <div className="myProfile-info-container">
-
-                        <div className="myProfile-info-phoneNumber">
-                            {user.phoneNumber}
-                        </div>
-                        <div className="myProfile-info-address-container">
-                            <div className="myProfile-info-address-streetAddress">
-                                {user.streetAddress}
+                    <div className="myProfile-contact-info-container">
+                        <div className="myProfile-contact-info-header">
+                            Contact Info
                             </div>
-                            <div className="myProfile-info-address-city">
-                                {user.city}
-                            </div>
-                            <div className="myProfile-info-address-zipCode">
-                                {user.zipCode}
-                            </div>
+                        <Divider/>
+                        <div className="myProfile-contact-info-container-sub">
+
+                        <TextField
+                                className="myProfile-info"
+                                id="phoneNumber-read-only-input"
+                                label="Phone Number"
+                                defaultValue={user.phoneNumber}
+                                InputProps={{
+                                    readOnly: true,
+                                }}
+                                sx={{margin: 1}}
+                                variant="filled"                            
+                            />
+                            <TextField
+                                className="myProfile-info"
+                                id="streetAddress-read-only-input"
+                                label="Street Address"
+                                defaultValue={user.streetAddress}
+                                InputProps={{
+                                    readOnly: true,
+                                }}
+                                sx={{margin: 1}}
+                                variant="filled"                            
+                            />
+                            <TextField
+                                className="myProfile-info"
+                                id="city-read-only-input"
+                                label="City"
+                                defaultValue={user.city}
+                                InputProps={{
+                                    readOnly: true,
+                                }}
+                                sx={{margin: 1}}
+                                variant="filled"                            
+                            />
+                            <TextField
+                                className="myProfile-info"
+                                id="zipcode-read-only-input"
+                                label="Zipcode"
+                                defaultValue={user.zipCode}
+                                InputProps={{
+                                    readOnly: true,
+                                }}
+                                sx={{margin: 1}}
+                                variant="filled"                            
+                            />
                         </div>
                     </div>
                 </div>
