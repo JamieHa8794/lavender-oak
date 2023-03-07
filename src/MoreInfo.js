0c3f7aa97d5a5c43285c6c7a5b27dcc799d07736
JamieHa8794 <JamieHa8794@gmail.com>
2023-03-07 17:31:22 -0500

    changed import of faker

--- src/MoreInfo.js
+++ src/MoreInfo.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import {faker} from '@faker-js/faker'
+import { faker } from '@faker-js/faker';
 
 import Accordion from '@mui/material/Accordion';
 import AccordionDetails from '@mui/material/AccordionDetails';
